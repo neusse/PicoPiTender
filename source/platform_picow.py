3649a4c6501c7369a39a75a73236fe60d173959a
neusse <neusse@gmail.com>
2022-10-04 17:41:16 -0700

    Create platform_picow.py

---
+++ source/platform_picow.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# *****************************************
+# PiTender RaspberryPi Interface Library
+# *****************************************
+#
+# Description: This library supports controlling the PiTender outputs via
+#  Raspberry Pi GPIOs
+#
+# *****************************************
+
+# *****************************************
+# Imported Libraries
+# *****************************************
+
+# we will not be using the gpio from here.
+# we will be making calls to the picoW instead using phew.
+# there are still some setup aptions that need to be configured in the rest of the code.
+# and most of this code will become a unique animal.
+
+
+# no need for this here since we will not be running on an Rpi here.
+#import RPi.GPIO as GPIO
+
+class PumpControl:
+	def __init__(self, settings):
+		# Init GPIO's to default values / behavior
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BCM)
+		self.pump_pins = {}
+		for pump_number, pin_number in settings['assignments'].items():
+			GPIO.setup(pin_number, GPIO.OUT, initial=1)
+			self.pump_pins[pump_number] = pin_number
+			print(f"Pin number {pin_number} initialized as output for {pump_number}.  Set to 1. ")
+
+	def ActivatePump(self, pump_number):
+		GPIO.output(self.pump_pins[pump_number], 0) # Turn on Relay
+		print(f"{pump_number} Pump Activated. Dispensing on pin: {self.pump_pins[pump_number]}")
+
+	def DeActivatePump(self, pump_number):
+		GPIO.output(self.pump_pins[pump_number], 1) # Turn off Relay
+		print(f"{pump_number} Pump De-Activated. Stopped Dispensing on pin: {self.pump_pins[pump_number]}")
+
+	def GetOutputStatus(self):
+		self.current = {}
+		for pump_number in self.pump_pins.items():
+			self.current[pump_number] = GPIO.input(self.pump_pins[pump_number])
+		return self.current
+
+	def Cleanup(self):
+		GPIO.cleanup()
